3e72843e62bf7e77aea5c6da276b830eeb34b82d
GLADIATORsvg <147329862+GLADIATORsvg@users.noreply.github.com>
2025-03-23 16:31:00 +0200

    Update webpack.config.js

--- ui/webpack.config.js
+++ ui/webpack.config.js
@@ -1,5 +1,21 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
-
+  entry: './src/index.js',
+  output: {
+    filename: 'app.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html'
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        { from: './src/style.css', to: 'css/style.css' }
+      ]
+    })
+  ]
 };
